f4abbb6b9ab469c03d037979310cf3f919116b5f
Altraqos <angelslayer1999@gmail.com>
2019-12-12 19:17:28 +0100

    Updated the voiceRec
    Added a Default spell function for easy spell creation

--- UnityMultiplayerProject/Assets/Scripts/Spells/VoiceRecognition.cs
+++ UnityMultiplayerProject/Assets/Scripts/Spells/VoiceRecognition.cs
@@ -10,11 +10,11 @@ public class VoiceRecognition : MonoBehaviour
 {
     public ParticleSystem pSystem;
     public Text sText;
-    public float colorRed;
-    public float colorGreen;
-    public float colorBlue;
-    public float colorAlpha;
-    public float spellDuration = 2;
+    public float cRed;
+    public float cGreen;
+    public float cBlue;
+    public float cAlpha;
+    public float sDuration = 2;
     private KeywordRecognizer kRecognizer;
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
 
@@ -36,62 +36,94 @@ public class VoiceRecognition : MonoBehaviour
         actions[speech.text].Invoke();
     }
 
+    private void DefaultSpellFunction()
+    {
+        StopCoroutine("spellEffect");
+        cRed = 255;
+        cGreen = 255;
+        cBlue = 255;
+        cAlpha = 255;
+
+        /*
+         * Add the main spell functionality in here.
+         * Set the colours above to whatever the spell needs to be, add the real spell underneath.
+         * And finally start the spell coroutine.
+         */
+
+        StartCoroutine("spellEffect");
+    }
+    
     private void AvadaKedavra()
     {
         StopCoroutine("spellEffect");
-        colorRed = 0;
-        colorGreen = 255;
-        colorBlue = 0;
-        colorAlpha = 255;
+        cRed = 0;
+        cGreen = 255;
+        cBlue = 0;
+        cAlpha = 255;
+
+
+
         StartCoroutine("spellEffect");
     }
 
     private void Crucio()
     {
         StopCoroutine("spellEffect");
-        colorRed = 210;
-        colorGreen = 0;
-        colorBlue = 0;
-        colorAlpha = 255;
+        cRed = 210;
+        cGreen = 0;
+        cBlue = 0;
+        cAlpha = 255;
+
+
+
         StartCoroutine("spellEffect");
     }
 
     private void Aguamenti()
     {
         StopCoroutine("spellEffect");
-        colorRed = 0;
-        colorGreen = 50;
-        colorBlue = 210;
-        colorAlpha = 255;
+        cRed = 0;
+        cGreen = 50;
+        cBlue = 210;
+        cAlpha = 255;
+
+
+
         StartCoroutine("spellEffect");
     }
 
     private void Alohomora()
     {
         StopCoroutine("spellEffect");
-        colorRed = 255;
-        colorGreen = 255;
-        colorBlue = 0;
-        colorAlpha = 255;
+        cRed = 255;
+        cGreen = 255;
+        cBlue = 0;
+        cAlpha = 255;
+
+
+
         StartCoroutine("spellEffect");
     }
 
     private void Reducto()
     {
         StopCoroutine("spellEffect");
-        colorRed = 224;
-        colorGreen = 10;
-        colorBlue = 10;
-        colorAlpha = 255;
+        cRed = 224;
+        cGreen = 10;
+        cBlue = 10;
+        cAlpha = 255;
+
+
+
         StartCoroutine("spellEffect");
     }
 
     public IEnumerator spellEffect()
     {
         var main = pSystem.main;
-        main.startColor = new Color(colorRed, colorGreen, colorBlue, colorAlpha);
+        main.startColor = new Color(cRed, cGreen, cBlue, cAlpha);
         pSystem.Play();
-        yield return new WaitForSecondsRealtime(spellDuration);
+        yield return new WaitForSecondsRealtime(sDuration);
         pSystem.Stop();
         StopCoroutine("spellEffect");
     }
